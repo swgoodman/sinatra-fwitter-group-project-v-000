124551881078d273456dd69f301100235d32d2a3
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-17 17:50:10 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       session[:user_id] = @user.id
       redirect '/tweets'
-      binding.pry
     else
       redirect '/signup'
     end
