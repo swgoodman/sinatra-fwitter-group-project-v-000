96a2058c4ce4ed62254bf0cbc3bf8735493c974b
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-17 02:08:27 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,10 @@
 class TweetsController < ApplicationController
 
-
+  get '/tweets' do
+    if !logged_in?
+      redirect '/login'
+    else
+      erb :'tweets/tweets'
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+  
 
+  get '/signup' do
+    if !session[:user_id]
+      erb :'users/signup'
+    else
+      redirect '/tweets'
+    end
+  end
 
+  post '/signup' do
+    if !params.values.include? ""
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
 end
