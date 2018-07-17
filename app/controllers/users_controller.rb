582bf548ac8b3645ecf80b02ebe220baa439d694
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-17 18:15:56 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,4 +44,9 @@ class UsersController < ApplicationController
       redirect '/'
     end
   end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :"users/show"
+  end
 end
