c83fbbd660fbfd795a5beb6a6fc959723ed866ba
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-17 18:02:44 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   post '/signup' do
     if !params.values.include? ""
-      @user = User.create(username: params[:username], email: params[:email], password: params[:password_digest])
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       session[:user_id] = @user.id
       redirect '/tweets'
     else
